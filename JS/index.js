e22eaa940e6201b126ebe9d3f7cb19110d4d6b07
Sohrab Hossain Tosher <sohrabhossain207@gmail.com>
2024-03-18 16:03:19 +0600

    header slider

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+document.getElementById('bf-add').addEventListener('click', function () {
+    let bfAdd = document.getElementById('bf-add');
+    bfAdd.innerHTML = `
+        <i class="p-2 bg-body fa-solid fa-heart"></i>
+    `
+
+})
